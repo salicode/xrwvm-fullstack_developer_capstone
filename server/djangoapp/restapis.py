83fd12c96a3aeeef30287934da3b7460931ce732
Salisu Aminu Abdurrahman <salisuaminu615@gmail.com>
2025-06-03 03:50:33 -0400

    fixed indentation

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,70 +1,8 @@
-# # Uncomment the imports below before you add the function code
-# # import requests
-# import os
-# from dotenv import load_dotenv
+# Uncomment the imports below before you add the function code
 # import requests
-
-# load_dotenv()
-
-# backend_url = os.getenv(
-#     'backend_url', default="http://localhost:3030")
-# sentiment_analyzer_url = os.getenv(
-#     'sentiment_analyzer_url',
-#     default="http://localhost:5050/")
-
-# # def get_request(endpoint, **kwargs):
-# # Add code for get requests to back end
-
-# def get_request(endpoint, **kwargs):
-#     params = ""
-#     if(kwargs):
-#         for key,value in kwargs.items():
-#             params=params+key+"="+value+"&"
-
-#     request_url = backend_url+endpoint+"?"+params
-
-#     print("GET from {} ".format(request_url))
-#     try:
-#         # Call get method of requests library with URL and parameters
-#         response = requests.get(request_url)
-#         return response.json()
-#     except:
-#         # If any error occurs
-#         print("Network exception occurred")
-
-# # def analyze_review_sentiments(text):
-# # request_url = sentiment_analyzer_url+"analyze/"+text
-# # Add code for retrieving sentiments
-
-
-# def analyze_review_sentiments(text):
-#     request_url = sentiment_analyzer_url+"analyze/"+text
-#     try:
-#         # Call get method of requests library with URL and parameters
-#         response = requests.get(request_url)
-#         return response.json()
-#     except Exception as err:
-#         print(f"Unexpected {err=}, {type(err)=}")
-#         print("Network exception occurred")
-
-# # def post_review(data_dict):
-# # Add code for posting review
-
-# def post_review(data_dict):
-#     request_url = backend_url+"/insert_review"
-#     try:
-#         response = requests.post(request_url,json=data_dict)
-#         print(response.json())
-#         return response.json()
-#     except:
-#         print("Network exception occurred")
-
-# #Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
-
 import os
 from dotenv import load_dotenv
 import requests
-
 
 load_dotenv()
 
@@ -74,27 +12,25 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
-
 def get_request(endpoint, **kwargs):
     params = ""
-    if kwargs:
-        for key, value in kwargs.items():
-            params = params + key + "=" + value + "&"
+    if(kwargs):
+        for key,value in kwargs.items():
+            params=params+key+"="+value+"&"
 
-    request_url = backend_url + endpoint + "?" + params
+    request_url = backend_url+endpoint+"?"+params
 
     print("GET from {} ".format(request_url))
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except Exception:
+    except:
         # If any error occurs
         print("Network exception occurred")
 
-
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url + "analyze/" + text
+    request_url = sentiment_analyzer_url+"analyze/"+text
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
@@ -103,16 +39,14 @@ def analyze_review_sentiments(text):
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
-
 def post_review(data_dict):
-    request_url = backend_url + "/insert_review"
+    request_url = backend_url+"/insert_review"
     try:
-        response = requests.post(request_url, json=data_dict)
+        response = requests.post(request_url,json=data_dict)
         print(response.json())
         return response.json()
-    except Exception:
+    except:
         print("Network exception occurred")
 
+#Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
 
-# Update the `get_dealerships` render list of dealerships all by default,
-# particular state if state is passed
